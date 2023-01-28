2764f65af0bde00a9cf98c4ad8eb5eda3a868afc
Kee Myoung Nam <kmnam.99.12@gmail.com>
2023-01-28 21:10:17 +0900

    Debugging

--- include/polytopes.hpp
+++ include/polytopes.hpp
@@ -286,7 +286,10 @@ std::vector<Simplex> getFullDimFaces(Delaunay_triangulation& tri)
             // Get the coordinates of the i-th vertex 
             Point p = it->vertex(i)->point();
             for (int j = 0; j < dim; ++j)
-                face_vertex_coords(i, j) = static_cast<mpq_rational>(CGAL::to_double(p[j]));  
+            {
+                std::cout << p[j] << " " << CGAL::to_double(p[j]) << " " << static_cast<mpq_rational>(CGAL::to_double(p[j])) << " " << static_cast<mpq_rational>(p[j]) << std::endl;
+                face_vertex_coords(i, j) = static_cast<mpq_rational>(p[j]);
+            } 
         }
         faces.emplace_back(Simplex(face_vertex_coords));
     }
@@ -319,8 +322,8 @@ std::vector<Simplex> getFullDimFaces(Delaunay_triangulation* tri)
             Point p = it->vertex(i)->point();
             for (int j = 0; j < dim; ++j)
             {
-                std::cout << p[j] << " " << CGAL::to_double(p[j]) << " " << static_cast<mpq_rational>(CGAL::to_double(p[j])) << std::endl;
-                face_vertex_coords(i, j) = static_cast<mpq_rational>(CGAL::to_double(p[j]));
+                std::cout << p[j] << " " << CGAL::to_double(p[j]) << " " << static_cast<mpq_rational>(CGAL::to_double(p[j])) << " " << static_cast<mpq_rational>(p[j]) << std::endl;
+                face_vertex_coords(i, j) = static_cast<mpq_rational>(p[j]);
             } 
         }
         faces.emplace_back(Simplex(face_vertex_coords));
